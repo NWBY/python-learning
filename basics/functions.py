f4581107066b34fcb60e3d1f33c689eb3e5a0225
NWBY <sam.newby19@gmail.com>
2022-02-01 19:16:29 +0000

    Functions

--- basics/functions.py
+++ basics/functions.py
@@ -1,10 +1,21 @@
 # To define a function need to use the `def` keyword
-def printUsersName():
+# Functions names should be all lowercase with underscores
+def print_users_name():
     name = input('Enter your name: ')
     print('Hello ' + name + "!")
     
 # To run a function we need to call it like below
-# printUsersName()
+# print_users_name()
 
 # Functions can have parameters
+def say_planet(planet):
+    print(planet)
+    
+# say_planet('Mars')
 
+# Functions can also return something
+def get_squared(num):
+    return num ** 2
+
+squared = get_squared(3)
+print(squared)

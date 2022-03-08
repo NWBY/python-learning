06814e210f02aedde266431272c93257e5e2fec0
NWBY <sam.newby19@gmail.com>
2022-03-08 20:12:34 +0000

    More tests for storing and show tokens

--- full-stack-api/tests/test_tokens.py
+++ full-stack-api/tests/test_tokens.py
@@ -1,10 +1,41 @@
 import pytest
+import json
+
+from api.models.Token import Token
 
 def test_index(client):
     res = client.get('/tokens/')
     assert res.status_code == 200
 
 def test_store(client):
-    res = client.post('/tokens/', json={
-        'name': ''
-    })
+    data = {
+        'name': 'Algorand',
+        'unit_name': 'ALGO',
+        'description': 'Pure Proof of Stake blockchain with a focus on security and building a decentralised future with the prospect of handling 10K+ TPS.',
+        'total_supply': 10000000,
+        'market_cap': 1000000000
+    }
+    
+    res = client.post('/tokens/', json=data)
+    resJson = json.loads(res.data)
+    
+    assert res.status_code == 200
+    assert resJson == data
+    
+def test_show(app, client):
+    token = Token(
+        name='Algorand',
+        unit_name='ALGO',
+        description='Pure Proof of Stake blockchain with a focus on security and building a decentralised future with the prospect of handling 10K+ TPS.',
+        total_supply=10000000,
+        market_cap=1000000000
+    )
+    app.db.session.add(token)
+    app.db.session.commit()
+    
+    res = client.get('/tokens/ALGO')
+    resJson = json.loads(res.data)
+    
+    assert res.status_code == 200
+    assert resJson['name'] == 'Algorand'
+    
